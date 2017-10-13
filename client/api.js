a28fe120f4656f3be46633a513918bfff05004c6
Jimsher Skhirtladze <jskhirtladze@gmail.com>
2017-10-13 17:00:46 +0300

    changed stuff

--- client/api.js
+++ client/api.js
@@ -3,14 +3,35 @@ if (typeof global === 'undefined') {
   global = window;
 }
 
-/* write your code here */
+const fetchAll = new Promise(
+  function(resolve, reject) {
+    resolve(global.fetch('/tv').then(
+      (x)=>{
+        //return xxx;
+        return global.fetch('/drink').then(
+          (y)=>{
+              return global.fetch('/food').then(
+                (z)=>{
+                  return [x,y,z];
+                }
+              );
+          }
+        );
+      }
+    ));
+  }
+);
 
+const api = { get: {}, go: {} };
 
-
-// define the api-client object to be exported
-const api = {
-  
-};
+api.get = (q) => global.fetch('/'+q);
+api.get.me = (q) => global.fetch('/'+q);
+api.get.me.a = (q) => global.fetch('/'+q);
+api.get.me.everything = () => fetchAll;
+api.go.get = (q) => global.fetch('/'+q);
+api.go.get.me = (q) => global.fetch('/'+q);
+api.go.get.me.a = (q) => global.fetch('/'+q);
+api.go.get.me.everything = () => fetchAll;
 
 // making the code work both in the browser and node.js
 if (typeof module !== 'undefined') { module.exports = api; }
--- server/api.js
+++ server/api.js
@@ -1,5 +1,35 @@
 const api = require('express')();
 
-/* your code here */
+let cnt = 10;
+
+api.get('/food',(req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    if (!cnt) {
+      res.send(JSON.stringify({error:'error'}));
+      return;
+    }
+    cnt--;
+    res.send(JSON.stringify({food:'bread'}));
+});
+
+api.get('/drink',(req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    if (!cnt) {
+      res.send(JSON.stringify({error:'error'}));
+      return;
+    }
+    cnt--;
+    res.send(JSON.stringify({drink:'cola'}));
+});
+
+api.get('/tv',(req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    if (!cnt) {
+      res.send(JSON.stringify({error:'error'}));
+      return;
+    }
+    cnt--;
+    res.send(JSON.stringify({tv:'samsung'}));
+});
 
 module.exports = api;
